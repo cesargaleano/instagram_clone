a3f981c7ef730eebfac6e8508439331d4a549a30
cesargaleano <87844938+cesargaleano@users.noreply.github.com>
2022-07-02 10:54:40 -0500

    Finish Home View (Visual)

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,17 +7,21 @@ import { ProductCard } from '../components/product/ProductCard';
 import { Stories } from '../components/home/Stories';
 import { Post } from '../components/home/Post';
 import {POSTS} from '../mockups/posts';
+import TabButtons from '../components/home/TabButtons';
+import { Head } from '../components/home/Head';
 
 export const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-        <Header />
+        <Head />
         <Stories />
         <ScrollView vertical>
         {POSTS.map((post,index)=>(
             <Post key={index} post={post}/>
         ))}
     </ScrollView>
+    <TabButtons />
+
     </SafeAreaView>
   )
 }
